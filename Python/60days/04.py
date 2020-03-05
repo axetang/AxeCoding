05c60881e142cf7bd28da07fd21aea1f70cc3677
Axe Tang <axetang@163.com>
2020-03-05 22:03:21 +0800

    04

--- Python/60days/04.py
+++ Python/60days/04.py
@@ -7,21 +7,21 @@ def is_duplicate(lst):
 
 
 a = [1, -2, 3, 4, 1, 2]
+b = [x for x in a if a.count(x) == 1]
+print("b is ", b)
 print(is_duplicate(a))
-
-# 反转列表
 
 
 def reverse_list(lst):
+    # 反转列表
     return lst[::-1]
 
 
 print(reverse_list(a), a)
 
-'''找出重复元素'''
-
 
 def find_duplicate(lst):
+    # 找出重复元素
     ret = []
     for i in lst:
         if lst.count(i) > 1 and i not in ret:
@@ -33,10 +33,9 @@ def find_duplicate(lst):
 r = find_duplicate([1, 2, 3, 4, 3, 2])
 print(r)
 
-# 斐波那契数列
-
 
 def fibonaci(n):
+    # 斐波那契数列 1
     if n <= 1:
         return [1]
     fib = [1, 1]
@@ -45,17 +44,20 @@ def fibonaci(n):
     return fib
 
 
+print("Fibonaci func 1")
 f = fibonaci(10)
 print(f)
 
 
 def fibo(n):
+    # 斐波那契数列 2
     a, b = 1, 1
     for _ in range(n):
         yield a
         a, b = b, a+b
 
 
+print("Fibonaci func 2:生成器")
 print(list(fibo(2)))
 print(list(fibo(5)))
 
@@ -63,14 +65,23 @@ print(list(fibo(5)))
 def mode(lst):
     if lst is None or len(lst) == 0:
         return None
-    return max(lst, key=lambda v: lst.count(v))
+    max_frequent_elem = max(lst, key=lambda v: lst.count(v))
+    max_frequent = lst.count(max_frequent_elem)
+    ret = []
+    for i in lst:
+        if i not in ret and lst.count(i) == max_frequent:
+            ret.append(i)
+    return ret
 
 
-lst = [1, 3, 3, 2, 1, 1, 2]
+lst = [1, 3, 3, 3, 2, 1, 1, 2]
+m = max(lst)
+n = max(1, 3, 5)
 r = mode(lst)
+print(m, n)
 print(f'{lst} 中出现次数最多的元素为:{r}')
 
-
+'''
 # 返回表头
 def head(lst):
     return lst[0] if len(lst) > 0 else None
@@ -100,3 +111,4 @@ def mul_table():
 
 
 mul_table()
+'''
